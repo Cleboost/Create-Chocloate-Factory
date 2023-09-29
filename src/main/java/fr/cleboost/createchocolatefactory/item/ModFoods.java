508db5bd9bf544db11aef72f2d455b45191e43e5
Cleboost <clement.balarot@gmail.com>
2023-09-29 21:24:14 +0200

    add mint food

--- src/main/java/fr/cleboost/createchocolatefactory/item/ModFoods.java
+++ src/main/java/fr/cleboost/createchocolatefactory/item/ModFoods.java
@@ -8,10 +8,10 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2).fast()
             .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
-
     public static final FoodProperties DARK_CHOCOLATE = new FoodProperties.Builder().nutrition(6).fast()
             .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 300), 0.4f).build();
-
     public static final FoodProperties BROWN_CHOCOLATE = new FoodProperties.Builder().nutrition(4).fast()
+            .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
+    public static final FoodProperties MINT = new FoodProperties.Builder().nutrition(4).fast()
             .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
 }
