6f29aea5b47b77c2aee2888ac60ee83c287246af
Youritch <86657843+Youritch@users.noreply.github.com>
2023-10-09 19:03:05 +0200

    for machete v2

--- src/main/java/fr/cleboost/createchocolatefactory/utils/ModTags.java
+++ src/main/java/fr/cleboost/createchocolatefactory/utils/ModTags.java
@@ -11,7 +11,8 @@ import net.minecraft.world.level.block.Block;
 public class ModTags {
     public static class Blocks {
 
-        public static final TagKey<Block> METAL_DETECTOR_VALUABLE = tag("metal_detector_valuable");
+        public static final TagKey<Block> MACHETE_BREAKABLE = tag("machete_breakable");
+        public static final TagKey<Block> MACHETE_CLEANABLE = tag("machete_cleanable");
 
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(CreateChocolateFactory.MOD_ID, name));
@@ -19,6 +20,7 @@ public class ModTags {
     }
 
     public static class Items {
+        public static final TagKey<Item> MACHETE_LIKE = tag("machete_like");
         private static TagKey<Item> tag(String name) {
             return ItemTags.create(new ResourceLocation(CreateChocolateFactory.MOD_ID, name));
         }
