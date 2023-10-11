98521b0d8dc13af92951c60f9f5532f83dd57fa0
Youritch <86657843+Youritch@users.noreply.github.com>
2023-10-11 18:33:20 +0200

    first test

---
+++ src/main/java/fr/cleboost/createchocolatefactory/item/TestFoodItem.java
@@ -0,0 +1,11 @@
+package fr.cleboost.createchocolatefactory.item;
+
+import net.minecraft.world.item.Item;
+
+public class TestFoodItem extends Item {
+    public TestFoodItem(Properties pProperties) {
+        super(pProperties);
+        this.getFoodProperties();
+    }
+
+}
