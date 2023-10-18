4213af873746d42dfce1a4790ad815d5f777126a
Cleboost <clement.balarot@gmail.com>
2023-10-18 22:01:58 +0200

    upgrade comments

--- src/main/java/fr/cleboost/createchocolatefactory/datagen/ModLangGenerator.java
+++ src/main/java/fr/cleboost/createchocolatefactory/datagen/ModLangGenerator.java
@@ -2,16 +2,17 @@ package fr.cleboost.createchocolatefactory.datagen;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 
 public class ModLangGenerator extends LanguageProvider {
     public ModLangGenerator(PackOutput output, String modid, String locale) {
@@ -22,18 +23,18 @@ public class ModLangGenerator extends LanguageProvider {
     protected void addTranslations() {
         String lang = this.getName().replace("Languages: ", "");
         try {
-            //get data from sheet
+            //Get data from sheet
             URL url = new URL("https://sheetdb.io/api/v1/ylmy7zgiwbjrq\n");
             URLConnection request = url.openConnection();
             request.connect();
             BufferedReader br = new BufferedReader(new InputStreamReader((InputStream) request.getContent()));
-            //get as JSON
+            //Get as JSON
             String data = br.readLine();
             Gson gson = new Gson();
             JsonArray translates = gson.fromJson(data, JsonArray.class);
             for (int i = 0; i < translates.size(); i++) {
                 JsonObject tr = translates.get(i).getAsJsonObject();
-                //get information
+                //Get information
                 String key = tr.get("Full Unique Name (auto)").getAsString();
                 String trans = tr.get(lang).getAsString();
                 this.add(key, trans);
