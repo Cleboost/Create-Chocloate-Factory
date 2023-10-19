d27458999cdd5cfb83ed80ec9eab42ec2f8adc85
Youritch <86657843+Youritch@users.noreply.github.com>
2023-10-19 19:42:00 +0200

    add getColor

--- src/main/java/fr/cleboost/createchocolatefactory/utils/Chocolate.java
+++ src/main/java/fr/cleboost/createchocolatefactory/utils/Chocolate.java
@@ -4,6 +4,7 @@ import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 
+import java.nio.ByteBuffer;
 import java.util.Objects;
 
 public class Chocolate {
@@ -12,8 +13,9 @@ public class Chocolate {
     private float sugar = 0F;
     private float cocoaButter = 0F;
 
-    //private/public ??? color;
     public Chocolate(CompoundTag tag) {
+        if (!tag.contains(CreateChocolateFactory.MOD_ID)) return;
+        if (!tag.getCompound(CreateChocolateFactory.MOD_ID).contains("chocolate")) return;
         CompoundTag info = tag.getCompound(CreateChocolateFactory.MOD_ID).getCompound("chocolate");
         this.strength = info.getFloat("strength");
         this.milk = info.getFloat("milk");
@@ -21,14 +23,13 @@ public class Chocolate {
         this.cocoaButter = info.getFloat("cocoaButter");
     }
 
+    public Chocolate() {
+    }
+
     public void addIngredients(String ingredient, int amount) {
         if (ingredient.matches("^(strenght|milk|sugar|cocoaButter)$")) return;
-        if (
-            boolToInt(ingredient.equals("strength")) * (amount + this.strength) > 100 ||
-            boolToInt(ingredient.equals("milk")) * (amount + this.milk) > 100 &&
-            boolToInt(ingredient.equals("sugar")) * (amount + this.sugar) > 100 &&
-            boolToInt(ingredient.equals("cocoaButter")) * (amount + this.cocoaButter) > 100
-        ) return;
+        if (boolToInt(ingredient.equals("strength")) * (amount + this.strength) > 100 || boolToInt(ingredient.equals("milk")) * (amount + this.milk) > 100 && boolToInt(ingredient.equals("sugar")) * (amount + this.sugar) > 100 && boolToInt(ingredient.equals("cocoaButter")) * (amount + this.cocoaButter) > 100)
+            return;
         float coef = 1F - amount / 100F;
         this.strength = (coef * this.strength) + (amount * boolToInt(ingredient.equals("strength")));
         this.milk = (coef * this.milk) + (amount * boolToInt(ingredient.equals("milk")));
@@ -48,6 +49,13 @@ public class Chocolate {
         data.putFloat("sugar", this.sugar);
         data.putFloat("cocoaButter", this.cocoaButter);
         return new CompoundTag().put(CreateChocolateFactory.MOD_ID, Objects.requireNonNull(new CompoundTag().put("chocolate", data)));
+    }
+
+    public int getColor() {
+        //alpha red green blue
+        byte[] bytes = {120, (byte) (12.3*this.strength), 63, 0};
+        CreateChocolateFactory.LOGGER.info(String.valueOf(ByteBuffer.wrap(bytes).getInt()));
+        return ByteBuffer.wrap(bytes).getInt();
     }
 
     public int getCocoaButter() {
