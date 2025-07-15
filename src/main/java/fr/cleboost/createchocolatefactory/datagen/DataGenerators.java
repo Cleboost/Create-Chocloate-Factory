edc326cc0a32ba19893cc7df337424e2dd213cab
Cleboost <clement.balarot@gmail.com>
2025-07-15 19:38:34 +0200

    Remove unused imports and comments in DataGenerators
    Cleaned up the DataGenerators.java file by removing unused imports and a commented-out line related to HolderLookup and CompletableFuture. This improves code readability and maintainability.

--- src/main/java/fr/cleboost/createchocolatefactory/datagen/DataGenerators.java
+++ src/main/java/fr/cleboost/createchocolatefactory/datagen/DataGenerators.java
@@ -1,15 +1,12 @@
 package fr.cleboost.createchocolatefactory.datagen;
 
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.DataGenerator;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
-
-import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = CreateChocolateFactory.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
@@ -19,7 +16,6 @@ public class DataGenerators {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-        // CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(true, new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(true, new ModBlockStateProvider(packOutput, existingFileHelper));
