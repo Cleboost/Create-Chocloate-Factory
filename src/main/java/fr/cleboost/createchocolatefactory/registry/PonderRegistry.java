7c884015daa38e8088f35d1c8bcfd66a74f470b5
Cleboost <clement.balarot@gmail.com>
2025-07-17 02:02:14 +0200

    Rename PonderRegister to PonderRegistry
    Renamed the PonderRegister class and its file to PonderRegistry for improved naming consistency. Updated the class name accordingly.

--- src/main/java/fr/cleboost/createchocolatefactory/registry/PonderRegistry.java
+++ src/main/java/fr/cleboost/createchocolatefactory/registry/PonderRegistry.java
@@ -5,7 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import fr.cleboost.createchocolatefactory.ponder.scenes.DryingKitScene;
 import fr.cleboost.createchocolatefactory.utils.ModBlocks;
 
-public class PonderRegister {
+public class PonderRegistry {
     public static void register(PonderSceneRegistrationHelper<ResourceLocation> helper) {
         helper.forComponents(ModBlocks.DRYING_KIT.getId())
             .addStoryBoard("drying_kit", DryingKitScene::scene);
