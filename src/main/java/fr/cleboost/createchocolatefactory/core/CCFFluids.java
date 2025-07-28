db951c3fdbba67a454ea8150afc5561a99a71c82
Cleboost <clement.balarot@gmail.com>
2025-07-28 10:55:38 +0200

    Refactor chocolate fluid and add cocoa butter fluid
    Replaces the ChocolateFluid implementation with VirtualFluid for chocolate and introduces a new cocoa butter VirtualFluid. This simplifies fluid registration and adds support for cocoa butter in the mod.

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFFluids.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFFluids.java
@@ -5,20 +5,21 @@ import com.simibubi.create.content.fluids.VirtualFluid;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
-import fr.cleboost.createchocolatefactory.fluid.chocolate.ChocolateFluid;
 
 public class CCFFluids {
     private static final CreateRegistrate REGISTRATE = CreateChocolateFactory.registrate();
 
-    public static final FluidEntry<ChocolateFluid> CHOCOLATE =
-		REGISTRATE.virtualFluid("chocolate", 
-            CreateChocolateFactory.asResource("fluid/chocolate_still"), 
-            CreateChocolateFactory.asResource("fluid/chocolate_flow"), 
-            ChocolateFluid.ChocolateFluidType::new, ChocolateFluid::createSource, ChocolateFluid::createFlowing)
+    public static final FluidEntry<VirtualFluid> CHOCOLATE =
+		REGISTRATE.virtualFluid("chocolate")
             .tag(AllTags.commonFluidTag("chocolate"))
-            .properties(p -> p.viscosity(1000).density(1000))
             .lang("Molten Chocolate")
 			.register();
+
+    public static final FluidEntry<VirtualFluid> COCOA_BUTTER =
+        REGISTRATE.virtualFluid("cocoa_butter")
+            .tag(AllTags.commonFluidTag("cocoa_butter"))
+            .lang("Cocoa Butter")
+            .register();
 
     public static void register() {}
 }
