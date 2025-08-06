a2772beb56775f74e59297a78c642d5d1d0f678c
youritch <youri.duret@gmail.com>
2025-08-06 23:36:49 +0200

    maj valeur

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFFoods.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFFoods.java
@@ -5,16 +5,11 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class CCFFoods {
-    public static final FoodProperties DARK_CHOCOLATE = new FoodProperties.Builder().nutrition(4)
-            .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 300), 0.3f).build();
-    public static final FoodProperties BROWN_CHOCOLATE = new FoodProperties.Builder().nutrition(4)
-            .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.3f).build();
     public static final FoodProperties MINT = new FoodProperties.Builder().nutrition(0).fast()
-            .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
+            .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 200), 0.1f).build();
     public static final FoodProperties ORANGE_WEDGE = new FoodProperties.Builder().nutrition(1).fast()
             .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.8f).build();
     public static final FoodProperties CHOCOLATE_FAST = new FoodProperties.Builder().fast().build();
     public static final FoodProperties CHOCOLATE_SLOW = new FoodProperties.Builder().build();
     public static final FoodProperties COCOA_BUTTER = new FoodProperties.Builder().saturationModifier(0.2f).nutrition(6).build();
-    public static final FoodProperties CHOCOLATE_BAR = new FoodProperties.Builder().nutrition(4).saturationModifier(0.2f).build();
 }
