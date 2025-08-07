ca25489cf003c473f248b38dd280ae1b7d709fb8
youritch <youri.duret@gmail.com>
2025-08-07 18:36:10 +0200

    ajout registrykey taste

--- src/main/java/fr/cleboost/createchocolatefactory/CreateChocolateFactory.java
+++ src/main/java/fr/cleboost/createchocolatefactory/CreateChocolateFactory.java
@@ -2,18 +2,25 @@ package fr.cleboost.createchocolatefactory;
 
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.foundation.data.CreateRegistrate;
-
 import fr.cleboost.createchocolatefactory.block.kinetic.chocolateMixer.ChocolateMixerBlockEntity;
 import fr.cleboost.createchocolatefactory.core.*;
 import fr.cleboost.createchocolatefactory.datagen.CCFDatagen;
+import fr.cleboost.createchocolatefactory.datagen.CCFDatapackProvider;
+import fr.cleboost.createchocolatefactory.utils.Taste;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.slf4j.Logger;
+
+import java.util.Set;
 
 @Mod(CreateChocolateFactory.MODID)
 public class CreateChocolateFactory {
@@ -38,12 +45,24 @@ public class CreateChocolateFactory {
         CCFLootModifiers.register(modEventBus);
 
         modEventBus.addListener(CreateChocolateFactory::registerCapabilities);
+        modEventBus.addListener(CreateChocolateFactory::registerDatapackRegistries);
         modEventBus.addListener(CCFDatagen::gatherData);
     }
 
     @SubscribeEvent
-    public static void registerCapabilities(RegisterCapabilitiesEvent event){
+    public static void registerCapabilities(RegisterCapabilitiesEvent event) {
         ChocolateMixerBlockEntity.registerCapabilities(event);
+    }
+
+    @SubscribeEvent
+    public static void registerDatapackRegistries(DataPackRegistryEvent.NewRegistry event) {
+        // https://docs.neoforged.net/docs/concepts/registries/
+        event.dataPackRegistry(
+                CCFRegistryKeys.TASTE_REGISTRY_KEY,
+                Taste.CODEC,
+                Taste.CODEC,
+                tasteRegistryBuilder -> tasteRegistryBuilder.maxId(256)
+        );
     }
 
     @SubscribeEvent
---
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFRegistryKeys.java
@@ -0,0 +1,10 @@
+package fr.cleboost.createchocolatefactory.core;
+
+import fr.cleboost.createchocolatefactory.utils.Taste;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+public class CCFRegistryKeys {
+    public static final ResourceKey<Registry<Taste>> TASTE_REGISTRY_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath("taste",""));
+}
