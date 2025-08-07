104591dde7a678201e560b7782b15ace588a6332
youritch <youri.duret@gmail.com>
2025-08-07 18:39:27 +0200

    clean up

--- src/main/java/fr/cleboost/createchocolatefactory/datagen/CCFDatagen.java
+++ src/main/java/fr/cleboost/createchocolatefactory/datagen/CCFDatagen.java
@@ -1,7 +1,5 @@
 package fr.cleboost.createchocolatefactory.datagen;
 
-import java.util.concurrent.CompletableFuture;
-
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import fr.cleboost.createchocolatefactory.datagen.recipes.CCFStandardRecipeGen;
 import net.minecraft.core.HolderLookup;
@@ -9,17 +7,20 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
+import java.util.concurrent.CompletableFuture;
+
 public class CCFDatagen {
     public static void gatherData(GatherDataEvent event) {
         if (!event.getMods().contains(CreateChocolateFactory.MODID)) return;
-        
+
         DataGenerator gen = event.getGenerator();
         PackOutput packOutput = gen.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-		//ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        //ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
-        gen.addProvider(true, new CCFStandardRecipeGen(packOutput, lookupProvider));
-        
+        gen.addProvider(event.includeServer(), new CCFStandardRecipeGen(packOutput, lookupProvider));
+        gen.addProvider(event.includeServer(), new CCFDatapackProvider(packOutput, lookupProvider));
+
         if (event.includeServer()) {
             CCFRecipeProvider.registerAllProcessing(gen, packOutput, lookupProvider);
         }
