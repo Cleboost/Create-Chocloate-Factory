1f7eb99cecb409d618e75ebb7a81e6b6803e194b
youritch <youri.duret@gmail.com>
2025-08-11 17:33:46 +0200

    fix some value and alway edible for chocolate

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFFoods.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFFoods.java
@@ -9,7 +9,7 @@ public class CCFFoods {
             .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 200), 0.1f).build();
     public static final FoodProperties ORANGE_WEDGE = new FoodProperties.Builder().nutrition(1).fast()
             .saturationModifier(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.8f).build();
-    public static final FoodProperties CHOCOLATE_FAST = new FoodProperties.Builder().fast().build();
-    public static final FoodProperties CHOCOLATE_SLOW = new FoodProperties.Builder().build();
-    public static final FoodProperties COCOA_BUTTER = new FoodProperties.Builder().saturationModifier(0.2f).nutrition(6).build();
+    public static final FoodProperties CHOCOLATE_FAST = new FoodProperties.Builder().fast().alwaysEdible().build();
+    public static final FoodProperties CHOCOLATE_SLOW = new FoodProperties.Builder().alwaysEdible().build();
+    public static final FoodProperties COCOA_BUTTER = new FoodProperties.Builder().saturationModifier(2f).nutrition(6).build();
 }
