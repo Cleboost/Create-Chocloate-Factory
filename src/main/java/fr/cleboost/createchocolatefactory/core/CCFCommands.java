8e6311a5bc85d0a72dc2b8f3fab6c6d7cba8f4c3
youritch <youri.duret@gmail.com>
2025-08-11 17:33:19 +0200

    add command to commonEvents

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFCommands.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFCommands.java
@@ -1,4 +1,13 @@
 package fr.cleboost.createchocolatefactory.core;
 
+
+import com.mojang.brigadier.CommandDispatcher;
+import fr.cleboost.createchocolatefactory.command.ChocolateCommand;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+
 public class CCFCommands {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext buildContext) {
+        ChocolateCommand.register(dispatcher, buildContext);
+    }
 }
--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFCommonEvents.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFCommonEvents.java
@@ -1,4 +1,13 @@
 package fr.cleboost.createchocolatefactory.core;
 
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+
+@EventBusSubscriber
 public class CCFCommonEvents {
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent event) {
+        CCFCommands.register(event.getDispatcher(), event.getBuildContext());
+    }
 }
