7beb4c3bc654a625912d0d1bed37244c52186a2c
youritch <youri.duret@gmail.com>
2025-08-11 11:51:49 +0200

    fix registry Ressource location

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFRegistryKeys.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFRegistryKeys.java
@@ -1,11 +1,10 @@
 package fr.cleboost.createchocolatefactory.core;
 
-import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import fr.cleboost.createchocolatefactory.utils.Taste;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
 public class CCFRegistryKeys {
-    public static final ResourceKey<Registry<Taste>> TASTE_REGISTRY_KEY = ResourceKey.createRegistryKey(CreateChocolateFactory.asResource("taste"));
+    public static final ResourceKey<Registry<Taste>> TASTE_REGISTRY_KEY = ResourceKey.createRegistryKey(ResourceLocation.withDefaultNamespace("taste"));
 }
