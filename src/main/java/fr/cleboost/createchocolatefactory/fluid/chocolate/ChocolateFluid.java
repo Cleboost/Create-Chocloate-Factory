d4d2dbed6f9dfdfd55abce0a539ba27516d4ab32
youritch <youri.duret@gmail.com>
2025-08-12 14:42:00 +0200

    some work on fluid (bucket?)

--- src/main/java/fr/cleboost/createchocolatefactory/fluid/chocolate/ChocolateFluid.java
+++ src/main/java/fr/cleboost/createchocolatefactory/fluid/chocolate/ChocolateFluid.java
@@ -2,39 +2,59 @@ package fr.cleboost.createchocolatefactory.fluid.chocolate;
 
 import com.simibubi.create.AllFluids.TintedFluidType;
 import com.simibubi.create.content.fluids.VirtualFluid;
-
+import fr.cleboost.createchocolatefactory.core.CCFDataComponents;
+import fr.cleboost.createchocolatefactory.core.CCFItems;
+import fr.cleboost.createchocolatefactory.utils.Chocolate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.FluidState;
 import net.neoforged.neoforge.fluids.FluidStack;
 
 public class ChocolateFluid extends VirtualFluid {
     public ChocolateFluid(Properties properties, boolean source) {
-		super(properties, source);
-	}
+        super(properties, source);
+    }
+
+    @Override
+    public Item getBucket() {
+        return CCFItems.CHOCOLATE_BUCKET.get();
+    }
 
     public static ChocolateFluid createSource(Properties properties) {
-		return new ChocolateFluid(properties, true);
-	}
+        return new ChocolateFluid(properties, true);
+    }
 
-	public static ChocolateFluid createFlowing(Properties properties) {
-		return new ChocolateFluid(properties, false);
-	}
+    public static ChocolateFluid createFlowing(Properties properties) {
+        return new ChocolateFluid(properties, false);
+    }
 
     public static class ChocolateFluidType extends TintedFluidType {
-		public ChocolateFluidType(Properties properties, ResourceLocation stillTexture, ResourceLocation flowingTexture) {
-			super(properties, stillTexture, flowingTexture);
-		}
-
-        @Override
-        public int getTintColor(FluidStack stack) {
-            return 0xFFFF00FF;
+        public ChocolateFluidType(Properties properties, ResourceLocation stillTexture, ResourceLocation flowingTexture) {
+            super(properties, stillTexture, flowingTexture);
         }
 
         @Override
-		protected int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos) {
-			return NO_TINT;
-		}
+        public ItemStack getBucket(FluidStack fluidStack) {
+            ItemStack itemStack = super.getBucket(fluidStack);
+            if (fluidStack.has(CCFDataComponents.CHOCOLATE)) {
+                itemStack.set(CCFDataComponents.CHOCOLATE, fluidStack.get(CCFDataComponents.CHOCOLATE));
+            }
+            return itemStack;
+        }
+
+        @Override
+        public int getTintColor(FluidStack stack) {
+            if (!stack.has(CCFDataComponents.CHOCOLATE)) return NO_TINT;
+            Chocolate ch = stack.get(CCFDataComponents.CHOCOLATE);
+            return ch.getColor();
+        }
+
+        @Override
+        protected int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos) {
+            return NO_TINT;
+        }
     }
 }
