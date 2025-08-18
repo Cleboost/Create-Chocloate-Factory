c0d7d52101b764c5500caaf33d38427e10e5f417
youritch <youri.duret@gmail.com>
2025-08-18 12:03:19 +0200

    désctiver la recipe

--- src/main/java/fr/cleboost/createchocolatefactory/datagen/recipes/CCFEmptyingRecipeGen.java
+++ src/main/java/fr/cleboost/createchocolatefactory/datagen/recipes/CCFEmptyingRecipeGen.java
@@ -4,20 +4,21 @@ import java.util.concurrent.CompletableFuture;
 
 import com.simibubi.create.api.data.recipe.EmptyingRecipeGen;
 
+import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import fr.cleboost.createchocolatefactory.core.CCFFluids;
 import fr.cleboost.createchocolatefactory.core.CCFItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 
 public class CCFEmptyingRecipeGen extends EmptyingRecipeGen {
-    GeneratedRecipe 
-    
+    /*GeneratedRecipe
     CHOCOLATE_BUCKET = create("chocolate_bucket", b -> b
-        .require(CCFItems.CHOCOLATE_BUCKET)
+        .require(Ingredient.of(CCFFluids.CHOCOLATE.get().getBucket()))
         .output(Items.BUCKET)
-        .output(CCFFluids.CHOCOLATE.get(), 1000));
+        .output(CCFFluids.CHOCOLATE.get(), 1000));*/
 
     public CCFEmptyingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, CreateChocolateFactory.MODID);
