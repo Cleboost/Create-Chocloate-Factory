32d24b169795af27a00c21ca9554d54eaff64fed
Cleboost <clement.balarot@gmail.com>
2025-08-24 21:38:08 +0200

    feat: chocolate mixer ponder

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFPounders.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFPounders.java
@@ -1,5 +1,6 @@
 package fr.cleboost.createchocolatefactory.core;
 
+import fr.cleboost.createchocolatefactory.ponder.scenes.ChocolateMixerScene;
 import fr.cleboost.createchocolatefactory.ponder.scenes.DryingKitScene;
 import net.createmod.ponder.api.registration.PonderSceneRegistrationHelper;
 import net.minecraft.resources.ResourceLocation;
@@ -8,5 +9,7 @@ public class CCFPounders {
     public static void register(PonderSceneRegistrationHelper<ResourceLocation> helper) {
         helper.forComponents(CCFBlocks.DRYING_KIT.getId())
             .addStoryBoard("drying_kit", DryingKitScene::scene);
+        helper.forComponents(CCFBlocks.CHOCOLATE_MIXER.getId())
+            .addStoryBoard("chocolate_mixer", ChocolateMixerScene::scene);
     }
 }
---
+++ src/main/java/fr/cleboost/createchocolatefactory/ponder/scenes/ChocolateMixerScene.java
@@ -0,0 +1,68 @@
+package fr.cleboost.createchocolatefactory.ponder.scenes;
+
+import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
+import fr.cleboost.createchocolatefactory.core.CCFBlocks;
+import net.createmod.ponder.api.PonderPalette;
+import net.createmod.ponder.api.scene.SceneBuilder;
+import net.createmod.ponder.api.scene.SceneBuildingUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+
+public class ChocolateMixerScene {
+    public static void scene(SceneBuilder scene, SceneBuildingUtil util) {
+        scene.title("chocolate_mixer", CCFBlocks.CHOCOLATE_MIXER.asItem().getDescription().getString());
+        scene.configureBasePlate(0, 0, 5);
+
+        // Usefully position
+        BlockPos blazeBurnerBloc = util.grid().at(2, 1, 2);
+        BlockPos funnelBloc = util.grid().at(2, 2, 1);
+        BlockPos pipeOut =  util.grid().at(2, 2, 3);
+
+
+        scene.world().showSection(util.select().cuboid(util.grid().at(0, 0, 0), new Vec3i(5, 0, 4)), Direction.DOWN);
+        scene.idle(20);
+
+        scene.world().showSection(util.select().cuboid(blazeBurnerBloc, new Vec3i(0, 3, 0)), Direction.DOWN);
+        scene.idle(40);
+
+        scene.world().modifyBlock(blazeBurnerBloc, (state) -> state.setValue(BlazeBurnerBlock.HEAT_LEVEL, BlazeBurnerBlock.HeatLevel.KINDLED), true);
+        scene.overlay().chaseBoundingBoxOutline(PonderPalette.BLUE, blazeBurnerBloc, AABB.unitCubeFromLowerCorner(Vec3.atLowerCornerOf(blazeBurnerBloc)), 20);
+        scene.idle(20);
+        scene.overlay().showText(40).text("Heat...").attachKeyFrame().placeNearTarget().pointAt(new Vec3(blazeBurnerBloc.getX() + 0.5, blazeBurnerBloc.getY() + 0.15, blazeBurnerBloc.getZ() + 0.5));
+        scene.idle(40);
+
+        //Show Belt
+        scene.world().showSection(util.select().cuboid(util.grid().at(2, 1, 0), new Vec3i(0, 1, 1)), Direction.SOUTH);
+        scene.idle(20);
+        scene.overlay().showText(40).text("Input...").attachKeyFrame().placeNearTarget().pointAt(new Vec3(funnelBloc.getX() + 0.5, funnelBloc.getY() + 0.15, funnelBloc.getZ() + 0.5));
+        scene.idle(40);
+
+        //Show double tanks
+        scene.world().showSection(util.select().cuboid(util.grid().at(0, 1, 2), new Vec3i(1, 3, 0)), Direction.EAST);
+        scene.world().showSection(util.select().cuboid(util.grid().at(4, 1, 2), new Vec3i(-1, 3, 0)), Direction.WEST);
+        scene.idle(20);
+
+        //Show all mech
+        scene.world().showSection(util.select().cuboid(util.grid().at(3, 1, 0), new Vec3i(1, 0, 0)), Direction.WEST);
+        scene.world().showSection(util.select().cuboid(util.grid().at(1, 4, 3), new Vec3i(2, 0, 1)), Direction.WEST);
+
+        scene.idle(60);
+        //***********************
+        //TODO: Creation chocolat animation
+        //***********************
+
+        scene.rotateCameraY(180);
+        scene.idle(50);
+        scene.world().hideSection(util.select().cuboid(util.grid().at(0, 1, 2), new Vec3i(1, 3, 0)), Direction.WEST);
+        scene.world().hideSection(util.select().cuboid(util.grid().at(4, 1, 2), new Vec3i(-1, 3, 0)), Direction.EAST);
+        scene.world().hideSection(util.select().cuboid(util.grid().at(1, 4, 3), new Vec3i(2, 0, 1)), Direction.UP);
+
+        scene.idle(40);
+        scene.world().showSection(util.select().cuboid(util.grid().at(0, 1, 3), new Vec3i(2, 2, 1)), Direction.UP);
+        scene.overlay().showText(40).text("Out...").attachKeyFrame().placeNearTarget().pointAt(new Vec3(pipeOut.getX() + 0.5, pipeOut.getY() + 0.15, pipeOut.getZ() + 0.5));
+        scene.markAsFinished();
+    }
+}
