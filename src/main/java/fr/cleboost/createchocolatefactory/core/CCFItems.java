356bdb34c5736db08bb8f65c9670080591dec2e0
youritch <youri.duret@gmail.com>
2025-08-25 00:46:31 +0200

    ajout mould item

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFItems.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFItems.java
@@ -5,6 +5,7 @@ import com.tterrag.registrate.util.entry.ItemEntry;
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import fr.cleboost.createchocolatefactory.item.MacheteItem;
 import fr.cleboost.createchocolatefactory.item.utils.ChocolateBaseItem;
+import fr.cleboost.createchocolatefactory.item.utils.ChocolateMouldItem;
 import fr.cleboost.createchocolatefactory.item.utils.ChocolateProgressItem;
 import fr.cleboost.createchocolatefactory.utils.Chocolate;
 import net.minecraft.world.item.Item;
@@ -44,7 +45,7 @@ public class CCFItems {
     public static final ItemEntry<ChocolateBaseItem> CHOCOLATE_EGG = REGISTRATE.item("chocolate_egg", (properties) ->
             new ChocolateBaseItem(properties.food(CCFFoods.CHOCOLATE_SLOW).stacksTo(16), 400)).lang("Chocolate Item").register();
     public static final ItemEntry<ChocolateProgressItem> CHOCOLATE_BAR = REGISTRATE.item("chocolate_bar", (properties) ->
-                    new ChocolateProgressItem(properties.stacksTo(1).food(CCFFoods.CHOCOLATE_FAST), 3, 100f)).lang("Chocolate Bar")
+                    new ChocolateProgressItem(properties.stacksTo(1).food(CCFFoods.CHOCOLATE_FAST), 3, 100)).lang("Chocolate Bar")
             .model((ctx, prov) -> {
                 prov.generated(ctx, prov.modLoc("item/" + ctx.getName() + "/bar0"), prov.modLoc("item/" + ctx.getName() + "/bar_paper"))
                         //.texture("layer0", prov.modLoc("item/" + ctx.getName() + "/bar_paper"))
@@ -65,6 +66,9 @@ public class CCFItems {
                                         .texture("layer0", prov.modLoc("item/" + ctx.getName() + "/bar2"))
                         ).end();
             }).register();
+
+    //chocolate mould
+    public static final ItemEntry<ChocolateMouldItem> EGG_MOULD = REGISTRATE.item("egg_mould", (p) -> new ChocolateMouldItem(p, CHOCOLATE_EGG.get())).lang("Egg Mould").register();
 
     // Fruits & Foods
     public static final ItemEntry<Item> MINT_LEAF = REGISTRATE.item("mint_leaf", Item::new).properties(p -> p.food(CCFFoods.MINT)).register();
--- src/main/java/fr/cleboost/createchocolatefactory/item/utils/ChocolateMouldItem.java
+++ src/main/java/fr/cleboost/createchocolatefactory/item/utils/ChocolateMouldItem.java
@@ -1,4 +1,25 @@
 package fr.cleboost.createchocolatefactory.item.utils;
 
-public class ChocolateMouldItem {
+import fr.cleboost.createchocolatefactory.core.CCFDataComponents;
+import fr.cleboost.createchocolatefactory.utils.Chocolate;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class ChocolateMouldItem extends Item {
+    private final ChocolateBaseItem result;
+
+    public ChocolateMouldItem(Properties properties, ChocolateBaseItem result) {
+        super(properties.stacksTo(1));
+        this.result = result;
+    }
+
+    public ChocolateBaseItem getResultItem() {
+        return result;
+    }
+
+    public ItemStack getResult(Chocolate chocolate) {
+        ItemStack itemStack = new ItemStack(this.result);
+        itemStack.set(CCFDataComponents.CHOCOLATE.get(), chocolate);
+        return itemStack;
+    }
 }
