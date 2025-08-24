da511029087034e380dd2208207089078f1de0a4
youritch <youri.duret@gmail.com>
2025-08-24 20:29:07 +0200

    finish switiching to virtual

--- src/main/java/fr/cleboost/createchocolatefactory/core/CCFFluids.java
+++ src/main/java/fr/cleboost/createchocolatefactory/core/CCFFluids.java
@@ -7,24 +7,25 @@ import com.tterrag.registrate.util.entry.FluidEntry;
 import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import fr.cleboost.createchocolatefactory.block.MoltenChocolateBlock;
 import fr.cleboost.createchocolatefactory.fluid.ChocolateFluidType;
+import fr.cleboost.createchocolatefactory.fluid.VirtualChocolateFluid;
 import fr.cleboost.createchocolatefactory.item.ChocolateBucketItem;
 import net.neoforged.neoforge.fluids.BaseFlowingFluid;
 
 public class CCFFluids {
     private static final CreateRegistrate REGISTRATE = CreateChocolateFactory.registrate();
 
-    public static final FluidEntry<BaseFlowingFluid.Flowing> CHOCOLATE =
-            REGISTRATE.standardFluid("chocolate", ChocolateFluidType::new)
-                    .source(BaseFlowingFluid.Source::new)
-                    .block(MoltenChocolateBlock::new).build()
-                    .fluidProperties(p -> p.levelDecreasePerBlock(2).slopeFindDistance(2))
+    public static final FluidEntry<VirtualChocolateFluid> CHOCOLATE =
+            REGISTRATE.virtualFluid("chocolate", VirtualChocolateFluid.ChocolateFluidType::new,VirtualChocolateFluid::createSource,VirtualChocolateFluid::createFlowing)
+                    //.source(BaseFlowingFluid.Source::new)
+                    //.block(MoltenChocolateBlock::new).build()
+                    //.fluidProperties(p -> p.levelDecreasePerBlock(2).slopeFindDistance(2))
                     .tag(AllTags.commonFluidTag("chocolate"))
                     .lang("Molten Chocolate")
-                    .bucket(ChocolateBucketItem::new)
+                    /*.bucket(ChocolateBucketItem::new)
                         .lang("Bucket of Molten Chocolate")
                         .tag(AllTags.AllItemTags.UPRIGHT_ON_BELT.tag)
-                        .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/chocolate_bucket/overlay"), prov.modLoc("item/chocolate_bucket/bucket")))
-                    .build()
+                        .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/chocolate_bucket/overlay"), prov.modLoc("item/chocolate_bucket/bucket")))*/
+                    //.build()
                     .register();
 
     public static final FluidEntry<VirtualFluid> COCOA_BUTTER =
---
+++ src/main/java/fr/cleboost/createchocolatefactory/fluid/VirtualChocolateFluid.java
@@ -0,0 +1,63 @@
+package fr.cleboost.createchocolatefactory.fluid;
+
+import com.simibubi.create.AllFluids;
+import com.simibubi.create.content.fluids.VirtualFluid;
+import com.simibubi.create.content.fluids.potion.PotionFluid;
+import fr.cleboost.createchocolatefactory.block.MoltenChocolateBlock;
+import fr.cleboost.createchocolatefactory.core.CCFDataComponents;
+import fr.cleboost.createchocolatefactory.utils.Chocolate;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.material.FluidState;
+import net.neoforged.neoforge.fluids.BaseFlowingFluid;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.FluidType;
+
+public class VirtualChocolateFluid extends VirtualFluid {
+
+    public static VirtualChocolateFluid createSource(Properties properties) {
+        return new VirtualChocolateFluid(properties, true);
+    }
+
+    public static VirtualChocolateFluid createFlowing(Properties properties) {
+        return new VirtualChocolateFluid(properties, false);
+    }
+
+    public VirtualChocolateFluid(Properties properties, boolean source) {
+        super(properties, source);
+    }
+
+    public static class  ChocolateFluidType extends AllFluids.TintedFluidType {
+        public ChocolateFluidType(Properties properties, ResourceLocation stillTexture, ResourceLocation flowingTexture) {
+            super(properties, stillTexture, flowingTexture);
+        }
+    /*@Override
+    public ItemStack getBucket(FluidStack fluidStack) {
+        ItemStack itemStack = super.getBucket(fluidStack);
+        if (fluidStack.has(CCFDataComponents.CHOCOLATE)) {
+            itemStack.set(CCFDataComponents.CHOCOLATE, fluidStack.get(CCFDataComponents.CHOCOLATE));
+        }
+        return super.getBucket(fluidStack);
+    }*/
+
+        @Override
+        public int getTintColor(FluidStack stack) {
+            if (!stack.has(CCFDataComponents.CHOCOLATE)) return NO_TINT;
+            Chocolate ch = stack.get(CCFDataComponents.CHOCOLATE);
+            return ch.getColor();
+        }
+
+        @Override
+        protected int getTintColor(FluidState state, BlockAndTintGetter getter, BlockPos pos) {
+            Chocolate ch = MoltenChocolateBlock.getChocolate(pos);
+            //if (ch == null) return NO_TINT;
+            return ch.getColor();
+        }
+
+    /*@Override
+    public FluidState getStateForPlacement(BlockAndTintGetter getter, BlockPos pos, FluidStack stack) {
+        return super.getStateForPlacement(getter, pos, stack);
+    }*/
+    }
+}
