7c8257e7325a91e90320854c33821fd99eb0ca19
Cleboost <clement.balarot@gmail.com>
2025-08-28 15:01:40 +0200

    feat: ad craft to all chocolate mould

--- src/main/java/fr/cleboost/createchocolatefactory/datagen/recipes/CCFStandardRecipeGen.java
+++ src/main/java/fr/cleboost/createchocolatefactory/datagen/recipes/CCFStandardRecipeGen.java
@@ -1,24 +1,31 @@
 package fr.cleboost.createchocolatefactory.datagen.recipes;
 
 import com.simibubi.create.AllItems;
-import com.simibubi.create.AllTags;
+import com.tterrag.registrate.util.entry.ItemEntry;
+
 import fr.cleboost.createchocolatefactory.core.CCFBlocks;
 import fr.cleboost.createchocolatefactory.core.CCFItems;
+import fr.cleboost.createchocolatefactory.item.utils.ChocolateMouldItem;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.GameRules;
-import net.neoforged.neoforge.common.Tags;
 
 import javax.annotation.Nonnull;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class CCFStandardRecipeGen extends RecipeProvider {
     public CCFStandardRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries);
     }
+
+    static List<ItemEntry<ChocolateMouldItem>> allChocolateMouldItems = new ArrayList<>(
+        List.of(CCFItems.CHOCOLATE_EGG_PACK.getMouldItems())
+    );
 
     @Override
     public void buildRecipes(@Nonnull RecipeOutput consumer) {
@@ -54,5 +61,9 @@ public class CCFStandardRecipeGen extends RecipeProvider {
                 .pattern("S")
                 .unlockedBy("has_iron_sheet", has(AllItems.IRON_SHEET.get()))
                 .save(consumer, "machete");
+
+        for (ItemEntry<ChocolateMouldItem> item : allChocolateMouldItems) {
+            stonecutterResultFromBase(consumer, RecipeCategory.MISC, item, AllItems.COPPER_SHEET);
+        }
     }
 }
