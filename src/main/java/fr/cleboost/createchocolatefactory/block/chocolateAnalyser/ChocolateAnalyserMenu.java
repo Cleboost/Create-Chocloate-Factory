fba0bf44ef1eb460467bea71b77958df8ca9c8f7
Cleboost <clement.balarot@gmail.com>
2025-11-01 17:43:52 +0100

    refactor: update Chocolate Analyser menu and screen to use new layout and functionality

--- src/main/java/fr/cleboost/createchocolatefactory/block/chocolateAnalyser/ChocolateAnalyserMenu.java
+++ src/main/java/fr/cleboost/createchocolatefactory/block/chocolateAnalyser/ChocolateAnalyserMenu.java
@@ -2,8 +2,14 @@ package fr.cleboost.createchocolatefactory.block.chocolateAnalyser;
 
 import javax.annotation.Nonnull;
 
+import com.simibubi.create.foundation.gui.menu.MenuBase;
+
 import fr.cleboost.createchocolatefactory.core.CCFMenu;
 import fr.cleboost.createchocolatefactory.utils.OutputOnlySlot;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
@@ -15,24 +21,29 @@ import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
-public class ChocolateAnalyserMenu extends AbstractContainerMenu {
+public class ChocolateAnalyserMenu extends MenuBase<ChocolateAnalyserBlockEntity> {
 	private final Container container;
 	private final ContainerData data;
 
 	private static final int DATA_COOKING_PROGRESS = 2;
 	private static final int DATA_COOKING_TOTAL_TIME = 3;
+
+	private static final int OFFSET_X = 79-78+3;
+	private static final int OFFSET_Y = 37-105+16;
 	
-	public ChocolateAnalyserMenu(MenuType<?> type, int id, Inventory inv, ChocolateAnalyserBlockEntity be) {
+	public ChocolateAnalyserMenu(MenuType<?> type, int id, Inventory inv, @Nonnull ChocolateAnalyserBlockEntity be) {
 		super(type, id);
 		checkContainerSize(be, 3);
 		checkContainerDataCount(be, 4);
 		this.container = be;
 		this.data = be;
 		
-		this.addSlot(new Slot(be, ChocolateAnalyserInventory.SLOT_INPUT, 56, 17));
-		this.addSlot(new Slot(be, ChocolateAnalyserInventory.SLOT_FUEL, 56, 53));
-		this.addSlot(new OutputOnlySlot(be, ChocolateAnalyserInventory.SLOT_OUTPUT, 116, 35));
+		this.addSlot(new Slot(be, ChocolateAnalyserInventory.SLOT_INPUT, 39+OFFSET_X, 66+OFFSET_Y)); //Filter Input
+		this.addSlot(new Slot(be, ChocolateAnalyserInventory.SLOT_INPUT, 78+OFFSET_X, 27+OFFSET_Y)); //Chocolate Input
+		this.addSlot(new Slot(be, ChocolateAnalyserInventory.SLOT_FUEL, 78+OFFSET_X, 105+OFFSET_Y));
+		this.addSlot(new OutputOnlySlot(be, ChocolateAnalyserInventory.SLOT_OUTPUT, 117+OFFSET_X, 66+OFFSET_Y));
 		
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 9; ++j) {
@@ -48,19 +59,20 @@ public class ChocolateAnalyserMenu extends AbstractContainerMenu {
 	}
 	
 	public ChocolateAnalyserMenu(MenuType<?> type, int id, Inventory inv, RegistryFriendlyByteBuf buffer) {
-		this(type, id, inv, new SimpleContainer(3), new SimpleContainerData(4));
+		this(type, id, inv, new SimpleContainer(4), new SimpleContainerData(5));
 	}
 	
 	private ChocolateAnalyserMenu(MenuType<?> type, int id, Inventory inv, Container container, ContainerData data) {
 		super(type, id);
-		checkContainerSize(container, 3);
-		checkContainerDataCount(data, 4);
+		checkContainerSize(container, 4);
+		checkContainerDataCount(data, 5);
 		this.container = container;
 		this.data = data;
 		
-		this.addSlot(new Slot(container, ChocolateAnalyserInventory.SLOT_INPUT, 56, 17));
-		this.addSlot(new Slot(container, ChocolateAnalyserInventory.SLOT_FUEL, 56, 53));
-		this.addSlot(new OutputOnlySlot(container, ChocolateAnalyserInventory.SLOT_OUTPUT, 116, 35));
+		this.addSlot(new Slot(container, ChocolateAnalyserInventory.SLOT_INPUT, 39+OFFSET_X, 66+OFFSET_Y)); //Filter Input
+		this.addSlot(new Slot(container, ChocolateAnalyserInventory.SLOT_INPUT, 78+OFFSET_X, 27+OFFSET_Y)); //Chocolate Input
+		this.addSlot(new Slot(container, ChocolateAnalyserInventory.SLOT_FUEL, 78+OFFSET_X, 105+OFFSET_Y));
+		this.addSlot(new OutputOnlySlot(container, ChocolateAnalyserInventory.SLOT_OUTPUT, 117+OFFSET_X, 66+OFFSET_Y));
 		
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 9; ++j) {
@@ -114,13 +126,28 @@ public class ChocolateAnalyserMenu extends AbstractContainerMenu {
 	public boolean stillValid(@Nonnull Player player) {
 		return this.container.stillValid(player);
 	}
-
-	public int getProcessingProgress() {
-		return this.data.get(DATA_COOKING_PROGRESS);
-	}
-
-	public int getMaxProcessingTicks() {
-		return this.data.get(DATA_COOKING_TOTAL_TIME);
-	}
 	
+	@Override
+	protected void saveData(@Nonnull ChocolateAnalyserBlockEntity be) {
+
+	}
+
+	@Override
+	protected void initAndReadInventory(@Nonnull ChocolateAnalyserBlockEntity be) {
+	}
+		
+	@Override
+	protected ChocolateAnalyserBlockEntity createOnClient(RegistryFriendlyByteBuf extraData) {
+		BlockPos readBlockPos = extraData.readBlockPos();
+		CompoundTag readNbt = extraData.readNbt();
+
+		ClientLevel world = Minecraft.getInstance().level;
+		BlockEntity blockEntity = world.getBlockEntity(readBlockPos);
+		if (blockEntity instanceof ChocolateAnalyserBlockEntity c) {
+			c.readClient(readNbt, extraData.registryAccess());
+			return c;
+		}
+
+		return null;
+	}
 }
--- src/main/java/fr/cleboost/createchocolatefactory/block/chocolateAnalyser/ChocolateAnalyserScreen.java
+++ src/main/java/fr/cleboost/createchocolatefactory/block/chocolateAnalyser/ChocolateAnalyserScreen.java
@@ -2,19 +2,21 @@ package fr.cleboost.createchocolatefactory.block.chocolateAnalyser;
 
 import javax.annotation.Nonnull;
 
+import com.simibubi.create.foundation.gui.AllGuiTextures;
+import com.simibubi.create.foundation.gui.menu.AbstractSimiContainerScreen;
+
+import fr.cleboost.createchocolatefactory.core.CCFGuiTextures;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
-public class ChocolateAnalyserScreen extends AbstractContainerScreen<ChocolateAnalyserMenu> {
-    private static final ResourceLocation BACKGROUND = ResourceLocation.parse("minecraft:textures/gui/container/furnace.png");
+public class ChocolateAnalyserScreen extends AbstractSimiContainerScreen<ChocolateAnalyserMenu> {
 
     public ChocolateAnalyserScreen(ChocolateAnalyserMenu menu, Inventory inventory, Component title) {
 		super(menu, inventory, title);
-		this.imageWidth = 176;
-		this.imageHeight = 166;
+		this.imageWidth = 179;
+		this.imageHeight = 141;
 		this.inventoryLabelY = this.imageHeight - 94;
 		this.titleLabelY = 6;
 	}
@@ -22,25 +24,18 @@ public class ChocolateAnalyserScreen extends AbstractContainerScreen<ChocolateAn
     @Override
     protected void renderBg(@Nonnull GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
 		int i = (this.width - this.imageWidth) / 2;
-		int j = (this.height - this.imageHeight) / 2;
+		int h = (this.width - AllGuiTextures.PLAYER_INVENTORY.getWidth()) / 2;
+		int j = (this.height - (this.imageHeight + 5 + AllGuiTextures.PLAYER_INVENTORY.getHeight())) / 2;
 		
-		guiGraphics.blit(BACKGROUND, i, j, 0, 0, this.imageWidth, this.imageHeight);
-		
-		int processingProgress = this.menu.getProcessingProgress();
-		int maxProcessingTicks = this.menu.getMaxProcessingTicks();
-		if (processingProgress > 0 && maxProcessingTicks > 0) {
-			int flameHeight = 14;
-			int flameX = 56;
-			int flameY = 36;
-			guiGraphics.blit(BACKGROUND, i + flameX, j + flameY, 176, 0, 14, flameHeight);
-		}
+		guiGraphics.blit(CCFGuiTextures.CHOCOLATE_ANALYSER, i, j, 0, 0, this.imageWidth, this.imageHeight);
+	    renderPlayerInventory(guiGraphics, h, j + this.imageHeight + 5);
 	}
 
     @Override
     public void render(@Nonnull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-		this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+
 		super.render(guiGraphics, mouseX, mouseY, partialTick);
-		this.renderTooltip(guiGraphics, mouseX, mouseY);
+//		this.renderTooltip(guiGraphics, mouseX, mouseY);
 	}
 }
 
