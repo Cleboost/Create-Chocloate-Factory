9796cf542055df66fd71ae397764301bd0ac9c18
Cleboost <clement.balarot@gmail.com>
2026-06-30 15:05:47 +0200

    refactor(datagen): drop PLACED_FEATURE registration

--- src/main/java/fr/cleboost/createchocolatefactory/datagen/CCFDatapackProvider.java
+++ src/main/java/fr/cleboost/createchocolatefactory/datagen/CCFDatapackProvider.java
@@ -4,7 +4,6 @@ import fr.cleboost.createchocolatefactory.CreateChocolateFactory;
 import fr.cleboost.createchocolatefactory.core.CCFRegistryKeys;
 import fr.cleboost.createchocolatefactory.worldgen.CCFBiomeModifiers;
 import fr.cleboost.createchocolatefactory.worldgen.CCFConfiguredFeatures;
-import fr.cleboost.createchocolatefactory.worldgen.CCFPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -19,7 +18,6 @@ public class CCFDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(CCFRegistryKeys.TASTE_REGISTRY_KEY, TasteProvider::bootstrap)
             .add(Registries.CONFIGURED_FEATURE, CCFConfiguredFeatures::bootstrap)
-            .add(Registries.PLACED_FEATURE, CCFPlacedFeatures::bootstrap)
             .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, CCFBiomeModifiers::bootstrap);
 
     public CCFDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
